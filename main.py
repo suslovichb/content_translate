f9f5d325ed80ad67b84519dd044c06e12b451eb4
Bohdan Suslovich <suslovichb@gmail.com>
2020-03-11 14:08:58 +0200

    Add translation by enter hitting

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def fix_tags(text):
     text = text.replace('</ ', '</')
     return text
 
-def clicked():
+def translate(event=None):
     new_text = fix_tags(translator.translate([txt_original.get(1.0, END)], src='en', dest='uk')[0].text)
     txt_translated.delete(1.0, END)
     txt_translated.insert(1.0, new_text)
@@ -19,14 +19,13 @@ def clicked():
 window = Tk()
 window.title("Content Translate")
 window.geometry('500x400')
-
-# lbl = Label(window, text="testlabel1", font=("Arial Bold", 12))
-# lbl.grid(column=0, row=0)
+window.bind('<Return>', translate)
 
 txt_original = Text(window, width=50, height=10)
 txt_original.grid(column=0, row=1)
+txt_original.bind('<Return>', translate)
 
-btn = Button(window, text="translate", bg = 'grey', fg ='white', command=clicked)
+btn = Button(window, text="translate", bg = 'grey', fg ='white', command=translate)
 btn.grid(column=1, row=1)
 
 txt_translated = Text(window, width=50, height=10)
